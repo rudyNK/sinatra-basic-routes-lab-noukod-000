a281791b0989d98fbd77ba0a0b4877737296d9d7
Rudy Ravelin <rudy@noukod.com>
2019-01-10 14:30:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Rudy"
+  end
+
+  get '/hometown' do
+    "My hometown is Haiti"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Litty"
+  end
 end
